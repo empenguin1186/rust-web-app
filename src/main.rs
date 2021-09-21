4d9321f69ada2ff8677e27afa2b202ba1efebac2
empenguin1186 <emperorpenguint1186@gmail.com>
2021-09-21 23:50:16 +0900

    :+1: repository 完成

--- src/main.rs
+++ src/main.rs
@@ -36,17 +36,55 @@ mod repository;
 pub mod schema;
 use domain::posts_repository::PostsRepository;
 use repository::posts_repository_impl::PostsRepositoryImpl;
+use std::env::args;
+use std::io::{stdin, Read};
 
 fn main() {
-    // let connection = establish_connection();
-    // let repository = repository::posts_repository_impl::new_posts_repository_impl(connection);
-
     let repository = PostsRepositoryImpl::new();
-    let posts = repository.show_posts(true);
+    delete_post(repository)
+}
+
+fn show_posts(repository: PostsRepositoryImpl, is_published: bool) {
+    let posts = repository.show_posts(is_published);
 
     for post in posts {
         println!("{}", post.title);
         println!("--------------\n");
         println!("{}", post.body);
     }
+}
+
+fn write_post(repository: PostsRepositoryImpl) {
+    println!("What would you like your title to be?");
+    let mut title = String::new();
+    stdin().read_line(&mut title).unwrap();
+    let title = &title[..(title.len() - 1)];
+    println!("\nOk! Let's write {} (Press {} when finished\n", title, EOF);
+
+    let mut body = String::new();
+    stdin().read_to_string(&mut body).unwrap();
+
+    repository.write_post(title, &body);
+}
+
+#[cfg(not(windows))]
+const EOF: &'static str = "CTRL+D";
+
+#[cfg(windows)]
+const EOF: &'static str = "CTRL+Z";
+
+fn publish_post(repository: PostsRepositoryImpl) {
+    let update_id = args()
+        .nth(1)
+        .expect("publish_post requires a post id")
+        .parse::<i32>()
+        .expect("invalid id");
+
+    repository.publish_post(update_id);
+}
+
+fn delete_post(repository: PostsRepositoryImpl) {
+    let target = args().nth(1).expect("Expected a target to match against");
+
+    repository.delete_post(&target);
 }
--- src/repository/posts_repository_impl.rs
+++ src/repository/posts_repository_impl.rs
@@ -1,9 +1,9 @@
+use crate::schema::posts::dsl::{posts, published, title};
 use diesel::prelude::*;
 use diesel::MysqlConnection;
-use rust_web_app::schema::posts::dsl::{posts, published};
 
 use crate::domain::posts_repository::PostsRepository;
-use crate::models::Post;
+use crate::models::{NewPost, Post};
 use dotenv::dotenv;
 use std::env;
 
@@ -33,5 +33,32 @@ impl PostsRepository for PostsRepositoryImpl {
             .expect("Error loading posts");
 
         results
+    }
+
+    fn write_post<'a>(&self, post_title: &'a str, body: &'a str) {
+        let new_post = NewPost {
+            title: post_title,
+            body: body,
+        };
+
+        diesel::insert_into(posts)
+            .values(&new_post)
+            .execute(&self.connection)
+            .expect("Error saving new post");
+    }
+
+    fn publish_post(&self, update_id: i32) {
+        diesel::update(posts.find(update_id))
+            .set(published.eq(true))
+            .execute(&self.connection)
+            .expect("Error updating specified post");
+    }
+
+    fn delete_post(&self, word: &str) {
+        let pattern = format!("%{}%", word);
+
+        diesel::delete(posts.filter(title.like(pattern)))
+            .execute(&self.connection)
+            .expect("Error deleting posts");
     }
 }
