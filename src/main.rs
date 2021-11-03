856a1fb36119beb9696e686ee79bb80095bef1df
empenguin1186 <emperorpenguint1186@gmail.com>
2021-11-03 13:19:51 +0900

    :+1: Connection 周りの処理を途中まで実装

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ extern crate diesel;
 extern crate dotenv;
 extern crate serde_json;
 
+use std::borrow::Borrow;
 use std::env;
 use std::error::Error;
+use std::ops::Deref;
 
+use actix_web::{App, delete, get, HttpResponse, HttpServer, patch, post, Responder, web};
 use actix_web::web::Query;
-use actix_web::{delete, get, patch, post, web, App, HttpResponse, HttpServer, Responder};
-use diesel::prelude::*;
-use diesel::r2d2::ConnectionManager;
 use diesel::MysqlConnection;
+use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool};
 use serde::{Deserialize, Serialize};
+use serde_json::ser::State;
 
 use infrastructure::repository::posts_repository_impl::PostsRepositoryImpl;
 
@@ -26,117 +29,53 @@ mod infrastructure;
 pub mod models;
 pub mod schema;
 
-#[derive(Deserialize)]
-struct PostParam {
-    is_published: bool,
-}
-
-#[derive(Deserialize)]
-struct PatchParam {
-    id: i32,
-}
-
-#[derive(Deserialize)]
-struct DeleteParam {
-    keyword: String,
-}
-
-#[derive(Deserialize)]
-struct RequestPost {
-    title: String,
-    body: String,
-}
-
 #[derive(Serialize, Deserialize)]
-struct GetPostResponse {
-    results: Option<Vec<Post>>,
+struct CommentsResponse {
+    results: Option<Tree>,
     error: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
-struct CUDResponse {
-    result: Option<String>,
-    error: Option<String>,
+struct MyApp {
+    pool: Pool<ConnectionManager<MysqlConnection>>,
 }
 
-// type DbPool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
-
-// #[get("/comment/{comment_id}/child")]
-// async fn index(web::Path((comment_id)): web::Path<(u64)>, pool: web::Data<DbPool>) -> Result<HttpResponse, Error> {
-//     let conn = pool.get().expect("couldn't get db connection from pool");
-//     let comments = web::block(move || {
-//         let repository = CommentsRepositoryImpl::new(conn);
-//         let path = repository.get_path(comment_id);
-//         repository.select_comments(&path.unwrap().unwrap())
-//     })
-//         .await
-//         .map_err(|e| {
-//             eprintln!("{}", e);
-//             HttpResponse::InternalServerError().finish()
-//         })?;
-//     let tree = Tree::new(&comments);
-//     Ok(HttpResponse::Ok().json(tree))
-// }
-
-#[get("/post")]
-async fn get_posts(state: web::Data<PostState>, param: Query<PostParam>) -> impl Responder {
-    let is_published = param.is_published;
-    let result = state.get_posts(is_published);
+#[get("/comments")]
+async fn get_comments(app: web::Data<MyApp>) -> impl Responder {
+    let connection = app.pool.get()?;
+    // TODO まだ動かない
+    let repository = CommentsRepositoryImpl::new(connection.deref().to_owned());
+    let path = "1/".to_string();
+    let result = app.repository.select_comments(&path);
     match result {
-        Ok(n) => HttpResponse::Ok().json(GetPostResponse {
-            results: Some(n),
+        Ok(n) => HttpResponse::Ok().json(CommentsResponse {
+            results: Some(Tree::new(&n)),
             error: None,
         }),
-        Err(e) => HttpResponse::NotFound().json(GetPostResponse {
+        Err(e) => HttpResponse::NotFound().json(CommentsResponse {
             results: None,
             error: Some(format!("{:?}", e)),
         }),
     }
 }
 
-#[post("/post")]
-async fn post_post(state: web::Data<PostState>, request: web::Json<RequestPost>) -> impl Responder {
-    let result = state.post_post(&request.title, &request.body);
-    match result {
-        Ok(()) => HttpResponse::Ok().json(CUDResponse {
-            result: Some(String::from("Ok")),
-            error: None,
-        }),
-        Err(e) => HttpResponse::NotFound().json(CUDResponse {
-            result: None,
-            error: Some(format!("{:?}", e)),
-        }),
-    }
-}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<MysqlConnection>::new(database_url);
+    let pool = Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+    let bind = "127.0.0.1:8080";
 
-#[patch("/post")]
-async fn patch_post(state: web::Data<PostState>, param: Query<PatchParam>) -> impl Responder {
-    let result = state.patch_post(param.id);
-    match result {
-        Ok(()) => HttpResponse::Ok().json(CUDResponse {
-            result: Some(String::from("Ok")),
-            error: None,
-        }),
-        Err(e) => HttpResponse::NotFound().json(CUDResponse {
-            result: None,
-            error: Some(format!("{:?}", e)),
-        }),
-    }
-}
-
-#[delete("/post")]
-async fn delete_post(state: web::Data<PostState>, param: Query<DeleteParam>) -> impl Responder {
-    let result = state.delete_post(&param.keyword);
-    match result {
-        Ok(()) => HttpResponse::Ok().json(CUDResponse {
-            result: Some(String::from("Ok")),
-            error: None,
-        }),
-        Err(e) => HttpResponse::NotFound().json(CUDResponse {
-            result: None,
-            error: Some(format!("{:?}", e)),
-        }),
-    }
+    HttpServer::new(|| {
+        App::new()
+            .data(MyApp{ pool })
+            .service(get_comments)
+    })
+    .bind(&bind)?
+    .run()
+    .await
 }
 
 // fn main() {
@@ -173,57 +112,3 @@ async fn delete_post(state: web::Data<PostState>, param: Query<DeleteParam>) -> 
 //     }
 // }
 // }
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // let manager = ConnectionManager::<MysqlConnection>::new(database_url);
-    // let pool = r2d2::Pool::builder()
-    //     .build(manager)
-    //     .expect("Failed to create pool.");
-
-    // println!("Starting server at: {}", &bind);
-
-    let bind = "127.0.0.1:8080";
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let connection = MysqlConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url));
-    let repository = CommentsRepositoryImpl::new(connection);
-
-    HttpServer::new(move || {
-        App::new().app_data(pool.clone()).service(index)
-        // .service(get_posts)
-        // .service(post_post)
-        // .service(patch_post)
-        // .service(delete_post)
-    })
-    .bind(&bind)?
-    .run()
-    .await
-}
-
-pub struct PostState {
-    posts_service: Box<dyn PostsService>,
-}
-
-impl PostState {
-    async fn new(posts_service: Box<dyn PostsService>) -> PostState {
-        PostState { posts_service }
-    }
-
-    fn get_posts(&self, is_published: bool) -> Result<Vec<Post>, Box<dyn Error>> {
-        self.posts_service.read_posts(is_published)
-    }
-
-    fn post_post<'a>(&self, post_title: &'a str, body: &'a str) -> Result<(), Box<dyn Error>> {
-        self.posts_service.create_post(post_title, body)
-    }
-
-    fn patch_post(&self, update_id: i32) -> Result<(), Box<dyn Error>> {
-        self.posts_service.update_post(update_id)
-    }
-
-    fn delete_post(&self, word: &str) -> Result<(), Box<dyn Error>> {
-        self.posts_service.delete_post(word)
-    }
-}
